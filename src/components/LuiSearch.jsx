936cbe2623952b9b5804e34f960bada39f507468
Yianni Ververis <yiannisver@gmail.com>
2018-04-03 09:10:43 -0400

    0.0.3 add onClear and onType methods

--- src/components/LuiSearch.jsx
+++ src/components/LuiSearch.jsx
@@ -1,30 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-let inputSearch = null;
-const clear = (action) => {
-  inputSearch.value = '';
-  if (action) action();
+const LuiSearch = ({
+  placeholder, inverse, onClear, onType,
+}) => {
+  let inputSearch = null;
+  const clear = () => {
+    inputSearch.value = '';
+    if (onClear) onClear();
+  };
+  const change = (e) => {
+    if (onType) onType(e.key);
+  };
+  return (
+    <div className={(inverse) ? 'lui-search lui-search--inverse' : 'lui-search'} >
+      <span className="lui-icon  lui-icon--search  lui-search__search-icon" />
+      <input className="lui-search__input" maxLength="255" type="text" placeholder={placeholder} ref={elem => inputSearch = elem} onKeyPress={change} />
+      <button className="lui-search__clear-button" tabIndex={0} key="clear" onClick={clear}>
+        <span className="lui-icon  lui-icon--small  lui-icon--close" />
+      </button>
+    </div>
+  );
 };
-
-const LuiSearch = ({ placeholder, inverse, action }) => (
-  <div className={(inverse) ? 'lui-search lui-search--inverse' : 'lui-search'} >
-    <span className="lui-icon  lui-icon--search  lui-search__search-icon" />
-    <input className="lui-search__input" maxLength="255" spellCheck="false" type="text" placeholder={placeholder} ref={elem => inputSearch = elem} />
-    <button className="lui-search__clear-button" tabIndex={0} key="clear" onClick={() => clear(action)}>
-      <span className="lui-icon  lui-icon--small  lui-icon--close" />
-    </button>
-  </div>
-);
 LuiSearch.propTypes = {
   placeholder: PropTypes.string,
   inverse: PropTypes.bool,
-  action: PropTypes.func,
+  onClear: PropTypes.func,
+  onType: PropTypes.func,
 };
 LuiSearch.defaultProps = {
   placeholder: 'Search',
   inverse: false,
-  action: null,
+  onClear: null,
+  onType: null,
 };
 
 export default LuiSearch;
