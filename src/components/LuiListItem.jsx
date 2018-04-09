94c41c3526dd99670c521ea492739b41b66331d9
Francis Kabinoff <francis.kabinoff@gmail.com>
2018-04-09 13:56:10 -0400

    pass otherProps to LuiListItem

--- src/components/LuiListItem.jsx
+++ src/components/LuiListItem.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const LuiListItem = ({ children, onClick }) => (
-  <li className="lui-list__item" onClick={onClick}>
+const LuiListItem = ({ children, onClick, ...otherProps }) => (
+  <li className="lui-list__item" onClick={onClick} {...otherProps}>
     <span className="lui-list__text">{children}</span>
   </li>
 );
 LuiListItem.propTypes = {
   children: PropTypes.node,
   onClick: PropTypes.func,
+  otherProps: PropTypes.object,
 };
 LuiListItem.defaultProps = {
   children: null,
   onClick: null,
+  otherProps: null,
 };
 
 export default LuiListItem;
