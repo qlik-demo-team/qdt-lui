baf00030a07f25b0908fd81795c804897562b229
Yianni Ververis <yiannisver@gmail.com>
2018-04-23 14:21:22 -0400

    Merge and show x only when there is text

--- src/components/LuiSearch.jsx
+++ src/components/LuiSearch.jsx
@@ -3,22 +3,25 @@ import PropTypes from 'prop-types';
 
 const LuiSearch = ({
   clear, inverse, placeholder, value, ...otherProps
-}) => (
-  <div className={(inverse) ? 'lui-search lui-search--inverse' : 'lui-search'} >
-    <span className="lui-icon  lui-icon--search  lui-search__search-icon" />
-    <input
-      className="lui-search__input"
-      maxLength="255"
-      type="text"
-      placeholder={placeholder}
-      value={value}
-      {...otherProps}
-    />
-    <button className="lui-search__clear-button" tabIndex={0} key="clear" onClick={clear}>
-      <span className="lui-icon  lui-icon--small  lui-icon--close" />
-    </button>
-  </div>
-);
+}) => {
+  const button = (value.length) ? (<button className="lui-search__clear-button" tabIndex={0} key="clear" onClick={clear}>
+    <span className="lui-icon  lui-icon--small  lui-icon--close" />
+  </button>) : '';
+  return (
+    <div className={(inverse) ? 'lui-search lui-search--inverse' : 'lui-search'} >
+      <span className="lui-icon  lui-icon--search  lui-search__search-icon" />
+      <input
+        className="lui-search__input"
+        maxLength="255"
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        {...otherProps}
+      />
+      { button }
+    </div>
+  );
+};
 LuiSearch.propTypes = {
   clear: PropTypes.func.isRequired,
   inverse: PropTypes.bool,
